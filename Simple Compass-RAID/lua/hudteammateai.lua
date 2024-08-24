b46c5255490621d672ce5b9f169ebec3e92f48db
shinrax2 <shinra.ubu1@yahoo.de>
2024-08-24 14:55:58 +0200

    v8
    added more null checks

--- Simple Compass-RAID/lua/hudteammateai.lua
+++ Simple Compass-RAID/lua/hudteammateai.lua
@@ -1,6 +1,8 @@
 Hooks:PostHook(HUDTeammateAI, "set_name", "GTFO_Compass_Set_Ai_Name_Color", function(self, name)
     local unit = managers.criminals:character_unit_by_peer_id(self._peer_id)
-    local key = unit:key()
-    local panel = managers.hud._compass._teammate[key] and managers.hud._compass._teammate[key].panel
-    panel:child("compass_teammate_rect"):set_color(tweak_data.chat_colors[#tweak_data.chat_colors])
+    if unit then
+        local key = unit:key()
+        local panel = managers.hud._compass._teammate[key] and managers.hud._compass._teammate[key].panel
+        panel:child("compass_teammate_rect"):set_color(tweak_data.chat_colors[#tweak_data.chat_colors])
+    end
 end)
