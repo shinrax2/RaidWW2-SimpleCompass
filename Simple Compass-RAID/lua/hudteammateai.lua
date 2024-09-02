bcbd8611e90870df1aa01c3e9f86c96342b2f20c
shinrax2 <shinra.ubu1@yahoo.de>
2024-09-02 18:00:41 +0200

    POC

--- Simple Compass-RAID/lua/hudteammateai.lua
+++ Simple Compass-RAID/lua/hudteammateai.lua
@@ -4,5 +4,23 @@ Hooks:PostHook(HUDTeammateAI, "set_name", "GTFO_Compass_Set_Ai_Name_Color", func
         local key = unit:key()
         local panel = managers.hud._compass._teammate[key] and managers.hud._compass._teammate[key].panel
         panel:child("compass_teammate_rect"):set_color(tweak_data.chat_colors[#tweak_data.chat_colors])
+        panel:child("compass_teammate_icon"):set_color(tweak_data.chat_colors[#tweak_data.chat_colors])
+    end
+end)
+
+Hooks:PostHook(HUDTeammateAI, "set_nationality", "GTFO_Compass_Set_Ai_Name_Color", function(self, nationality)
+    local unit = managers.criminals:character_unit_by_peer_id(self._peer_id)
+    if unit then
+        for k, v in pairs(managers.hud._compass._teammate) do
+            if k == unit:key() then
+                managers.hud._compass._teammate[k].nationality = nationality
+                if managers.hud._compass.settings.TeammateIcon then
+                    local panel = managers.hud._compass._teammate[k].panel
+                    local icon = tweak_data.gui:get_full_gui_data("nationality_small_" .. nationality)
+                    panel:child("compass_teammate_icon"):set_image(icon.texture, unpack(icon.texture_rect))
+                end
+                return
+            end
+        end
     end
 end)
--- Simple Compass-RAID/lua/hudteammatepeer.lua
+++ Simple Compass-RAID/lua/hudteammatepeer.lua
@@ -11,6 +11,24 @@ Hooks:PostHook(HUDTeammatePeer, "set_name", "GTFO_Compass_Set_Peer_Name_Color", 
             end
             local crim_color = tweak_data.chat_colors[color_id] or tweak_data.chat_colors[#tweak_data.chat_colors]
             panel:child("compass_teammate_rect"):set_color(crim_color)
+            panel:child("compass_teammate_icon"):set_color(crim_color)
+        end
+    end
+end)
+
+Hooks:PostHook(HUDTeammatePeer, "set_nationality", "GTFO_Compass_Set_Ai_Name_Color", function(self, nationality)
+    local unit = managers.criminals:character_unit_by_peer_id(self._peer_id)
+    if unit then
+        for k, v in pairs(managers.hud._compass._teammate) do
+            if k == unit:key() then
+                managers.hud._compass._teammate[k].nationality = nationality
+                if managers.hud._compass.settings.TeammateIcon then
+                    local panel = managers.hud._compass._teammate[k].panel
+                    local icon = tweak_data.gui:get_full_gui_data("nationality_small_" .. nationality)
+                    panel:child("compass_teammate_icon"):set_image(icon.texture, unpack(icon.texture_rect))
+                end
+                return
+            end
         end
     end
 end)
